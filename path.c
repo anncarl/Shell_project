79f5f33c3c704715270471fbbb466e431c5c877f
NatashaMuhanji <natashamuhanji@gmail.com>
2023-07-13 09:52:19 -0700

    shell files

--- path.c
+++ path.c
@@ -1,4 +1,79 @@
-unsigned int i = 0, r = 0;
+#include "shell.h"
+
+/**
+ * path_execute - executes a command in the path
+ * @command: full path to the command
+ * @vars: pointer to struct of variables
+ *
+ * Return: 0 on succcess, 1 on failure
+ */
+int path_execute(char *command, vars_t *vars)
+{
+	pid_t child_pid;
+
+	if (access(command, X_OK) == 0)
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+			print_error(vars, NULL);
+		if (child_pid == 0)
+		{
+			if (execve(command, vars->av, vars->env) == -1)
+				print_error(vars, NULL);
+		}
+		else
+		{
+			wait(&vars->status);
+			if (WIFEXITED(vars->status))
+				vars->status = WEXITSTATUS(vars->status);
+			else if (WIFSIGNALED(vars->status) && WTERMSIG(vars->status) == SIGINT)
+				vars->status = 130;
+			return (0);
+		}
+		vars->status = 127;
+		return (1);
+	}
+	else
+	{
+		print_error(vars, ": Permission denied\n");
+		vars->status = 126;
+	}
+	return (0);
+}
+
+/**
+ * find_path - finds the PATH variable
+ * @env: array of environment variables
+ *
+ * Return: pointer to the node that contains the PATH, or NULL on failure
+ */
+char *find_path(char **env)
+{
+	char *path = "PATH=";
+	unsigned int i, j;
+
+	for (i = 0; env[i] != NULL; i++)
+	{
+		for (j = 0; j < 5; j++)
+			if (path[j] != env[i][j])
+				break;
+		if (j == 5)
+			break;
+	}
+	return (env[i]);
+
+}
+
+/**
+ * check_for_path - checks if the command is in the PATH
+ * @vars: variables
+ *
+ * Return: void
+ */
+void check_for_path(vars_t *vars)
+{
+	char *path, *path_dup = NULL, *check = NULL;
+	unsigned int i = 0, r = 0;
 	char **path_tokens;
 	struct stat buf;
 
