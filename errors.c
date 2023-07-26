64617293e32f0fd161fbad4294d2b137ce474f60
anncarl <mwendwaanncarl@gmail.com>
2023-07-26 01:26:07 -0700

    shell files

--- errors.c
+++ errors.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- * print_error - prints error messages to standard error
- * @vars: pointer to struct of variables
- * @msg: message to print
+ * print_error -this prints error messages to standard error
+ * @vars: a pointer to struct of variables
+ * @msg: the message to print
  *
  * Return: void
  */
@@ -27,8 +27,8 @@ void print_error(vars_t *vars, char *msg)
 }
 
 /**
- * _puts2 - prints a string to standard error
- * @str: string to print
+ * _puts2 - this prints a string to standard error
+ * @str: the string to print
  *
  * Return: void
  */
@@ -47,10 +47,10 @@ void _puts2(char *str)
 }
 
 /**
- * _uitoa - converts an unsigned int to a string
- * @count: unsigned int to convert
+ * _uitoa - this converts an unsigned int to a string
+ * @count: the unsigned int to convert
  *
- * Return: pointer to the converted string
+ * Return: a pointer to the converted string
  */
 char *_uitoa(unsigned int count)
 {
