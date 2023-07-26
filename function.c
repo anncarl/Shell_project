043eb160283985c83dee5db3c5c47dd1e06914bc
anncarl <mwendwaanncarl@gmail.com>
2023-07-26 01:30:38 -0700

    shell files

--- function.c
+++ function.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * _puts - writes a string to standard output
- * @str: string to write
+ * _puts -this writes a string to standard output
+ * @str: the string to write
  *
- * Return: number of chars printed or -1 on failure
+ * Return: the number of chars printed or -1 on failure
  */
 ssize_t _puts(char *str)
 {
@@ -21,9 +21,9 @@ ssize_t _puts(char *str)
 }
 
 /**
- * _strdup - returns pointer to new mem alloc space which contains copy
- * @strtodup: string to be duplicated
- * Return: a pointer to the new duplicated string
+ * _strdup - this returns pointer to new mem alloc space which contains copy
+ * @strtodup: the string to be duplicated
+ * Return: the pointer to the new duplicated string
  */
 char *_strdup(char *strtodup)
 {
@@ -45,10 +45,10 @@ char *_strdup(char *strtodup)
 }
 
 /**
- * _strcmpr - compares two strings
- * @strcmp1: first string, of two, to be compared in length
- * @strcmp2: second string, of two, to be compared
- * Return: 0 on success, anything else is a failure
+ * _strcmpr - this compares two strings
+ * @strcmp1: the first string, of two, to be compared in length
+ * @strcmp2: the second string, of two, to be compared
+ * Return: this returns 0 if it succeeds, and anything else is a failure
  */
 int _strcmpr(char *strcmp1, char *strcmp2)
 {
@@ -65,10 +65,10 @@ int _strcmpr(char *strcmp1, char *strcmp2)
 }
 
 /**
- * _strcat - concatenates two strings
- * @strc1: first string
- * @strc2: second string
- * Return: pointer
+ * _strcat - this concatenates two strings
+ * @strc1: the first string
+ * @strc2: the second string
+ * Return: the pointer
  */
 char *_strcat(char *strc1, char *strc2)
 {
@@ -105,9 +105,9 @@ char *_strcat(char *strc1, char *strc2)
 }
 
 /**
- * _strlen - returns the length of a string
- * @str: string to be measured
- * Return: length of string
+ * _strlen - this returns the length of a string
+ * @str: the string to be measured
+ * Return: returns the length of string
  */
 unsigned int _strlen(char *str)
 {
